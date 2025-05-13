0053fa285f8be5937d8f5942e5b90c81d55e3196
Nithya <nithya.jganesh@gmail.com>
2025-05-13 14:44:52 -0500

    Pdf Text Reader Commit

---
+++ PdfTextReader/PdfTextReader/PdfTextReader.py
@@ -0,0 +1,17 @@
+
+import fitz  # PyMuPDF
+
+def extract_text_from_pdf(pdf_path):
+    pdf_document = fitz.open(pdf_path)
+    text = ""
+
+    for page_num in range(len(pdf_document)):
+        page = pdf_document.load_page(page_num)
+        text += page.get_text()
+
+    return text
+
+if __name__ == "__main__":
+    pdf_file_path = r"C:\Users\djaya\Documents\Nithya\Python\Application-Form-PDF.pdf"
+    text = extract_text_from_pdf(pdf_file_path)
+    print(text)
