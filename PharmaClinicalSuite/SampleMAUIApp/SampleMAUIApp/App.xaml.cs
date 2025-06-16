90041e857153d0c6d5c95ed573b2dffb40004bdb
Nithya <nithya.jganesh@gmail.com>
2025-06-16 16:31:54 -0500

    feat(mdm):implements fuzzy matching logic for Participants

---
+++ PharmaClinicalSuite/SampleMAUIApp/SampleMAUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleMAUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
